a08110f4f5ba71d71300b1aab60d53490e916156
Prijam Khanal <71273444+Prijam-01@users.noreply.github.com>
2024-01-27 20:42:02 -0500

    client

---
+++ client/backend/models.py
@@ -0,0 +1,31 @@
+class User:
+    def __init__(self, user_id, username, email):
+        self.user_id = user_id
+        self.username = username
+        self.email = email
+
+class Instructor(User):
+    def __init__(self, user_id, username, email):
+        super().__init__(user_id, username, email)
+        self.classes = [Class_Instructor]
+
+class Student(User):
+    def __init__(self, user_id, username, email):
+        super().__init__(user_id, username, email)
+        self.classes = [Class_Student]
+
+class Class:
+    def __init__(self, class_id, class_name, professor):
+        self.class_id = class_id
+        self.class_name = class_name
+        self.professor = professor
+        self.std = []
+
+class Class_Instructor(Class):
+    def __init__(self, class_id, class_name, professor):
+        super().__init__(class_id, class_name, professor)
+
+class Class_Student(Class):
+    def __init__(self, class_id, class_name, professor):
+        super().__init__(class_id, class_name, professor)
+        
