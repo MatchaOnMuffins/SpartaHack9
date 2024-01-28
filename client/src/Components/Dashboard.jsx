6d073a4924327b11d95c1a5e172b27680ecc34ae
Yihang Zhu <yihangz@umich.edu>
2024-01-27 19:29:22 -0500

    Doing course page

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -2,21 +2,24 @@ import { useEffect, useState } from "react";
 import Header from "./Header";
 import axios from 'axios'
 import BasicCard from "./CourseElement";
+import { useAuth0 } from '@auth0/auth0-react';
+import CryptoJS from 'crypto-js';
 
 export default function Dashboard(){
-    
+    const { user, isAuthenticated, isLoading } = useAuth0();
     const[courseList,setCourseList] = useState([]);
-
     useEffect(()=>{
+        const hash = CryptoJS.MD5(user.email).toString();
+
         axios
-        .get('')
+        .get(`http://localhost:4000/user/${hash}`)
         .then((res)=>{
             setCourseList(res.data);
         })
         .catch((err)=>{
             console.error(err);
         })   
-    })
+    },[])
 
     return(<div>
         <Header/>
